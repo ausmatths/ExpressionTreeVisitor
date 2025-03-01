9bc7954ff76662611a0a96e8d06793c16bd257e1
Austin Matthews <114279644+ausmatths@users.noreply.github.com>
2025-02-28 21:18:26 -0800

    Fix destructor definition and accept method syntax in Multiplication class for C++11 compatibility

--- include/Elements/Multiplication.h
+++ include/Elements/Multiplication.h
@@ -12,11 +12,11 @@ private:
 
 public:
     Multiplication(std::shared_ptr<Expression> left, std::shared_ptr<Expression> right);
-    ~Multiplication() override = default;
+    virtual ~Multiplication() {}  // Changed syntax to fix C++11 issue
 
     const std::shared_ptr<Expression>& getLeft() const;
     const std::shared_ptr<Expression>& getRight() const;
-    void accept(Visitor& visitor) const override;
+    virtual void accept(Visitor& visitor) const;  // Removed override keyword
 };
 
 #endif // MULTIPLICATION_H
