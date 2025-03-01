19158ec34cbbb45c3d94eeb9614a52fad468e3e0
Austin Matthews <114279644+ausmatths@users.noreply.github.com>
2025-02-28 21:08:36 -0800

    Fix destructor definition in Expression class for C++11 compatibility

--- include/Expression.h
+++ include/Expression.h
@@ -1,12 +1,13 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
+// Forward declarations
 class Visitor;
 
 // Expression interface - the element that visitors will visit
 class Expression {
 public:
-    virtual ~Expression() = default;
+    virtual ~Expression() {}  // Changed from = default; to {} to fix C++11 issue
     virtual void accept(Visitor& visitor) const = 0;
 };
 
